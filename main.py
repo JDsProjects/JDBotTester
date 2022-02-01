2d23d10b1c774b05670797699be1cd6cc2b5502c
JDJG Inc. Official <ghostzap123@gmail.com>
2022-01-31 21:23:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import  os, traceback, logging
 import B, BotConfig
+import dotenv
 
 logging.basicConfig(level = logging.INFO)
 
 bot = BotConfig.bot
 
 B.b()
+dotenv.load_dotenv()
+
 bot.run(os.environ["TOKEN"])
