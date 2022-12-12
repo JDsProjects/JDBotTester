1a80e82426105890889e1c4a79c38561f6fd158d
JDJG Inc. Official <ghostzap123@gmail.com>
2022-12-11 22:39:18 -0500

    Removed python-dotenv

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ import logging
 import os
 import traceback
 
-import dotenv
-
 import BotConfig
 
 logging.basicConfig(level=logging.INFO)
 
 bot = BotConfig.bot
-
-dotenv.load_dotenv()
 
 bot.run(os.environ["TOKEN"])
